e6de399a4ec50c4078ce645a7aa548e78eae902c
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-04-18 16:51:19 -0300

    funcao de atualizar valor de item adicionada

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const lista = document.querySelector("#lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 itens.forEach((element) => {
-  criaElemento(element.nome, element.quantidade);
+  criaElemento(element);
 });
 
 formulario.addEventListener("submit", (event) => {
@@ -11,25 +11,38 @@ formulario.addEventListener("submit", (event) => {
   const nome = event.target.elements["nome"].value;
   const quantidade = event.target.elements["quantidade"].value;
 
-  armazenaDados(nome, quantidade);
-  criaElemento(nome, quantidade);
-  limpaFormulario(nome, quantidade, event);
+  const elementoExiste = itens.find((element) => element.nome === nome);
+
+  const itensObjeto = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+  if (elementoExiste) {
+    itensObjeto.id = elementoExiste.id;
+    itens[elementoExiste.id] = itensObjeto;
+    atualizaElemento(itensObjeto);
+  } else {
+    itensObjeto.id = itens.length;
+    criaElemento(itensObjeto);
+    itens.push(itensObjeto);
+  }
+
+  localStorage.setItem("itens", JSON.stringify(itens));
+  limpaFormulario(event);
   document.location.reload(true);
 });
 
 const apagar = document.querySelectorAll(".botaoApagar");
-console.log(apagar);
 apagar.forEach((Element, id) => {
   Element.addEventListener("click", (event) => {
-    console.log(apagar[id]);
-    console.log(itens);
     itens.splice(id, 1);
     localStorage.setItem("itens", JSON.stringify(itens));
     document.location.reload(true);
   });
 });
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
   const liItem = document.createElement("li");
   const divItem = document.createElement("div");
   const numeroItem = document.createElement("strong");
@@ -42,28 +55,24 @@ function criaElemento(nome, quantidade) {
   liItem.classList.add("item");
   botaoApagar.classList.add("botaoApagar");
   divItem.classList.add("divItens");
+  numeroItem.dataset.id = item.id;
 
-  numeroItem.innerHTML = quantidade;
-  divItem.innerHTML += nome;
+  numeroItem.innerHTML = item.quantidade;
+  divItem.innerHTML += item.nome;
 
   lista.appendChild(liItem);
 }
 
-function limpaFormulario(nome, quantidade, event) {
-  const elementos = event.target.elements;
+function limpaFormulario(formulario) {
+  const elementos = formulario.target.elements;
   for (i = 0; i < elementos.length; i++) {
     elementos[i].value = "";
   }
   elementos["adicionar"].value = "Adicionar";
 }
 
-function armazenaDados(nome, quantidade) {
-  const itensObjeto = {
-    nome: nome,
-    quantidade: quantidade,
-  };
-
-  itens.push(itensObjeto);
-
-  localStorage.setItem("itens", JSON.stringify(itens));
+function atualizaElemento(item) {
+  console.log(item.id);
+  document.querySelector("[data-id=" + '"' + item.id + '"]').innerHTML =
+    item.quantidade;
 }
